3d0a0bcb0ee89bd88f638c5207b806cd18b902ef
connectsilva <connectsilvaoficial@gmail.com>
2026-05-22 22:11:10 -0300

    v-0,002

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'capoeira-v1';
+const CACHE_NAME = 'capoeira-manager-v1';
+
 const urlsToCache = [
   './',
-  './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js',
-  'https://unpkg.com/html5-qrcode'
+  './index.html'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
